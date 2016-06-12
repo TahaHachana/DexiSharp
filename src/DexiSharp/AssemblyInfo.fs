3db5a61b47efbb4b36f98184191bea1d32309008
Taha <tahahachana@gmail.com>
2016-06-12 14:10:19 +0100

    Bump version to 0.0.2

--- src/DexiSharp/AssemblyInfo.fs
+++ src/DexiSharp/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("DexiSharp")>]
 [<assembly: AssemblyProductAttribute("DexiSharp")>]
 [<assembly: AssemblyDescriptionAttribute("F# client library for dexi.io")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.2")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
-    let [<Literal>] InformationalVersion = "0.0.1"
+    let [<Literal>] Version = "0.0.2"
+    let [<Literal>] InformationalVersion = "0.0.2"
